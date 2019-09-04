743f2ef296f8ecc9a2f226c21c8a161b9cb27b74
mx472756841 <xiangge1992>
2019-09-04 23:44:36 +0800

    角色路由管理

--- common/permission.py
+++ common/permission.py
@@ -1,3 +1,5 @@
+import json
+
 from app import db
 
 
@@ -26,20 +28,78 @@ def get_roles():
 
 
 def get_role_routes(role_id):
+    """
+    获取指定角色的路由，并按照上下级关系排列好
+    :param role_id:
+    :return:
+    """
     sql = """
-        select a.*, b.*
+        select 
+            a.id, a.path, a.name, a.component, a.alwaysShow,
+            a.redirect, a.meta, a.hidden
         from base_route_info a, base_role_route_info b
-        where a.id = b.route_id and b.role_id = :role_id
+        where a.id = b.route_id 
+        and b.role_id = :role_id
+        and a.parent_route_id is null
     """
 
     cur = db.session.execute(sql, {"role_id": role_id})
-    print(cur.fetchall())
-    return []
+    routes = cur.fetchall()
+    return_routes = []
+    for route in routes:
+        childs = get_child_routes(role_id, route[0])
+        data = {
+            "path": route[1],
+            "name": route[2],
+            "component": route[3],
+            "alwaysShow": True if route[4] else False,
+            "redirect": route[5],
+            "hidden": True if route[7] else False
+        }
+        if route[6]:
+            data['meta'] = json.loads(route[6])
+
+        if childs:
+            data['children'] = childs
+
+        return_routes.append(data)
+
+    return return_routes
 
 
-def get_routes():
+def get_child_routes(role_id, route_id):
     """
-    获取指定用户的routes
+    获取指定父级route和角色的routes
     :return:
     """
-    pass
+    sql = """
+        select 
+            a.id, a.path, a.name, a.component, a.alwaysShow,
+            a.redirect, a.meta, a.hidden
+        from base_route_info a, base_role_route_info b
+        where a.id = b.route_id 
+        and b.role_id = :role_id
+        and a.parent_route_id = :route_id
+    """
+    cur = db.session.execute(sql, {"role_id": role_id, "route_id": route_id})
+    routes = cur.fetchall()
+    return_routes = []
+    for route in routes:
+        # 继续递归获取当前的子集
+        childs = get_child_routes(role_id, route[0])
+        data = {
+            "path": route[1],
+            "name": route[2],
+            "component": route[3],
+            "alwaysShow": True if route[4] else False,
+            "redirect": route[5],
+            "hidden": True if route[7] else False
+        }
+        if route[6]:
+            data['meta'] = json.loads(route[6])
+
+        if childs:
+            data['children'] = childs
+
+        return_routes.append(data)
+    return return_routes
